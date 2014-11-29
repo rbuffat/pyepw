01c011ff92a4d984dbcfd1bc14a4ba320b631865
René Buffat <buffat@gmail.com>
2014-11-29 02:02:57 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup
-import os
-
 
 setup(name='pyepw',
       version='0.1',
       description='Python EPW file generator',
       url='https://github.com/rbuffat/pyepw',
-      download_url = 'https://github.com/rbuffat/pyepw/tarball/0.1',
+      download_url='https://github.com/rbuffat/pyepw/tarball/0.1',
       author='Rene Buffat',
       author_email='buffat@gmail.com',
       license='Apache License 2.0',
       classifiers=['Development Status :: 2 - Pre-Alpha'],
       packages=['pyepw'],
-      keywords = ['EnergyPlus', 'EPW'])
+      keywords=['EnergyPlus', 'EPW'])
