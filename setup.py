03b2bc6f6876d36b90e71397632a2814a1b515bd
René Buffat <buffat@gmail.com>
2014-11-29 02:23:36 +0100

    change desc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='pyepw',
       version='0.1',
-      description='Python EPW file generator',
+      description='Python EPW file reader and generator',
       url='https://github.com/rbuffat/pyepw',
       download_url='https://github.com/rbuffat/pyepw/tarball/0.1',
       author='Rene Buffat',
