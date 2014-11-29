78e59e1f3ae18bfa129048cbb408508e334cccdb
René Buffat <buffat@gmail.com>
2014-11-29 01:54:39 +0100

    cleanup

--- setup.py
+++ setup.py
@@ -2,21 +2,15 @@ from setuptools import setup
 import os
 
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(name='pyepw',
-      version='0.0.1-dev',
+      version='0.1',
       description='Python EPW file generator',
       url='https://github.com/rbuffat/pyepw',
+      download_url = 'https://github.com/rbuffat/pyepw/tarball/0.1',
       author='Rene Buffat',
       author_email='buffat@gmail.com',
       license='Apache License 2.0',
-      long_description=read('README.md'),
       classifiers=['Development Status :: 2 - Pre-Alpha'],
       packages=['pyepw'],
-      zip_safe=False)
+      keywords = ['EnergyPlus', 'EPW'], # arbitrary keywords
+      classifiers = [])
